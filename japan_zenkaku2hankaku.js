0cb0b30dc225a786fe179ad49ec822823ba4a710
risehou <38919094+risehou@users.noreply.github.com>
2021-12-17 18:08:42 +0900

    Create japan_zenkaku2hankaku.js

---
+++ japan_zenkaku2hankaku.js
@@ -0,0 +1,36 @@
+ function zenkaku2hankaku(id) {
+
+        var str = document.getElementById(id).value;
+
+        if (id == "TEL_1") {
+            str = str.replaceAll("-", "");
+            str = str.replaceAll("－", "");
+            str = str.replaceAll("/", "");
+            str = str.replaceAll("／", "");
+            str = str.replaceAll(" ", "");
+            str = str.replaceAll("　", "");
+            var out = str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
+                return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+            });
+            document.getElementById(id).value = out;
+        } else if (id == "YUBIN") {
+            str = str.replaceAll("-", "");
+            str = str.replaceAll("－", "");
+            str = str.replaceAll("/", "");
+            str = str.replaceAll("／", "");
+            if (str.length == 7) {
+                var out = str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
+                    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+                });
+                document.getElementById(id).value = out;
+            }
+        } else if ("Email") {
+            str = str.replaceAll(" ", "");
+            str = str.replaceAll("　", "");
+            
+            var out = str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
+                return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+            });
+            document.getElementById(id).value = out;
+
+        }
